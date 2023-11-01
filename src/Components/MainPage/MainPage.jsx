2d828a6315c0ab0321d45ce3b8bb8d31eff11da1
saumya200 <saumyaas2000@yahoo.com>
2023-11-01 10:05:32 +0530

    portfolio code changes

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -2,7 +2,7 @@
 import "./MainPage.css";
 import github from "../../Images/github.png";
 import linkedin from "../../Images/linkedin.png";
-import twitter from "../../Images/twitter.png";
+// import twitter from "../../Images/twitter.png";
 import saumya from "../../Images/saumya.png";
 // import saumya from "../../Images/thumbnail.jpeg";
 import "animate.css";
@@ -10,7 +10,7 @@ import "animate.css";
 function MainPage() {
   return (
     <div className="main--page">
-      <h1 className=" text-3xl font-bold text-black text-center mt-10">
+      <h1 className="main--page--top--name text-3xl font-bold text-black text-center mt-10">
         saumyareddy
       </h1>
       <div className="social--icons flex justify-center">
@@ -24,11 +24,9 @@ function MainPage() {
           <img src={linkedin} className="w-8 h-8 " />
         </a>
         {/* <img src={linkedin} className="w-8 h-8 " /> */}
-
-        <img src={twitter} className="w-10 h-10 ml-12" />
       </div>
       <div className="profile">
-        <img src={saumya} className="profile--image ml-20 mt-10" />
+        <img src={saumya} className="profile--image ml-12 mt-10" />
         <h1 className="font-bold text-3xl ml-8 mr-10 mt-8">
           Nice to meet you! I am
           <span className="profile--name underline decoration-4 decoration-green-400">
@@ -37,7 +35,7 @@ function MainPage() {
           </span>
         </h1>
         <p className="font-200 text-1xl ml-5 mt-5 mr-5">
-          Based in India, I am a software engineer and a passionate front-end
+          Based in India, I am a software engineer and a passionate fullstack
           developer, who loves to build websites that users love.
         </p>
         <a
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -2,7 +2,7 @@
 import "./Projects.css";
 import image1 from "../../Images/ss1.png";
 import image2 from "../../Images/ss2.png";
-import image4 from "../../Images/ss4.png";
+import image3 from "../../Images/ss4.png";
 
 function Projects() {
   return (
@@ -20,13 +20,13 @@ function Projects() {
           <div className="flex justify-center underline decoration-4 decoration-green-400 2xl:mr-28 ">
             <a href="https://my-travel-journal-theta.vercel.app/">
               {" "}
-              <p className="text-1xl font-bold  text mt-5 sm:ml-7 md:mr-5 2xl:pl-0">
+              <p className="projects--image1--para1 text-1xl font-bold ">
                 VIEW PROJECT
               </p>
             </a>
             <a href="https://github.com/saumya200/Travel-vite-">
               {" "}
-              <p className="text-1xl font-bold text mt-5 ml-10 md:mr-7 2xl:pl-0">
+              <p className="projects--image1--para2 text-1xl font-bold">
                 VIEW CODE{" "}
               </p>
             </a>
@@ -40,85 +40,31 @@ function Projects() {
             SAMPLE PRODUCT PAGE
           </h2>
           <div className=" image2--p flex justify-center underline decoration-4 decoration-green-400 ">
-            <p className="text-1xl font-bold  text mt-5 sm:ml-6 md:mr-7">
-              VIEW PROJECT
-            </p>
+            <p className="image2--para text-1xl font-bold">VIEW PROJECT</p>
             <a href="https://github.com/saumya200/Sample-Page-using-Tailwind-Css">
               {" "}
-              <p className="image2--view--code text-1xl font-bold text mt-5 ml-10 md:mr-7 lg:ml-52 2xl:ml-60 ">
+              <p className="image2--view--code text-1xl font-bold">
                 VIEW CODE{" "}
               </p>
             </a>
           </div>
         </div>
-        {/* <img src={image1} className="image1 mt-20 sm:ml-32 md:ml-44 lg:ml-20" />
-        <h2 className="text-3xl font-bold text-center mt-5 sm:ml-6 md:mr-10">
-          MY TRAVEL JOURNAL
-        </h2>
-        <div className="flex justify-center underline decoration-4 decoration-green-400 ">
-          <a href="https://my-travel-journal-theta.vercel.app/">
-            {" "}
-            <p className="text-1xl font-bold  text mt-5 sm:ml-7 md:mr-5">
-              VIEW PROJECT
-            </p>
-          </a>
-          <a href="https://github.com/saumya200/Travel-vite-">
-            {" "}
-            <p className="text-1xl font-bold text mt-5 ml-10 md:mr-7">
-              VIEW CODE{" "}
-            </p>
-          </a>
-        </div>
-
-        <img src={image2} className="image2 mt-20 sm:ml-20 md:ml-32" />
-        <h2 className="text-3xl font-bold text-center mt-5 sm:ml-6 md:mr-10">
-          SAMPLE PRODUCT PAGE
-        </h2>
-        <div className="flex justify-center underline decoration-4 decoration-green-400 ">
-          <p className="text-1xl font-bold  text mt-5 sm:ml-6 md:mr-7">
-            VIEW PROJECT
-          </p>
-          <a href="https://github.com/saumya200/Sample-Page-using-Tailwind-Css">
-            {" "}
-            <p className="text-1xl font-bold text mt-5 ml-10 md:mr-7">
-              VIEW CODE{" "}
-            </p>
-          </a>
-        </div> */}
         <div className="projects--image2">
           <img
-            src={image4}
+            src={image3}
             className="image3 mt-20 sm:ml-32 md:ml-44 xl:ml-80 2xl:ml-96"
           />
           <h2 className="image3--title text-3xl font-bold text-center mt-5 md:mr-8 xl:ml-10 2xl:mr-40">
             E-COMMERCE PRODUCT PAGE
           </h2>
           <div className=" image3--sub flex justify-center underline decoration-4 decoration-green-400 2xl:mr-28 ">
-            <p className="text-1xl font-bold  text mt-5 md:mr-6 xl:ml-10">
-              VIEW PROJECT
-            </p>
+            <p className="image3--para1 text-1xl font-bold">VIEW PROJECT</p>
             <a href="https://github.com/saumya200/shoe-store">
               {" "}
-              <p className="text-1xl font-bold text mt-5 ml-10 md:mr-6 xl:ml-10">
-                VIEW CODE{" "}
-              </p>
+              <p className="image3--para2 text-1xl font-bold">VIEW CODE </p>
             </a>
           </div>
         </div>
-
-        {/* <img src={image4} className="image3 mt-20 sm:ml-32 md:ml-44" />
-        <h2 className="text-3xl font-bold text-center mt-5 md:mr-8">
-          E-COMMERCE PRODUCT PAGE
-        </h2>
-        <div className="flex justify-center underline decoration-4 decoration-green-400 ">
-          <p className="text-1xl font-bold  text mt-5 md:mr-6">VIEW PROJECT</p>
-          <a href="https://github.com/saumya200/shoe-store">
-            {" "}
-            <p className="text-1xl font-bold text mt-5 ml-10 md:mr-6">
-              VIEW CODE{" "}
-            </p>
-          </a>
-        </div> */}
       </div>
     </div>
   );
